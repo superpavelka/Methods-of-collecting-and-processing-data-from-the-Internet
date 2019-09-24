a19b751b853b1b840ebe71479a557eb44fab4413
krilatiy <pos@list.ru>
2019-09-24 19:31:39 +0300

    5.2 beginning

--- Task5/jobparser/spiders/sj.py
+++ Task5/jobparser/spiders/sj.py
@@ -5,28 +5,27 @@ from scrapy.http import HtmlResponse
 from jobparser.items import JobparserItem
 import re
 
+
 class SjSpider(scrapy.Spider):
     name = 'sj'
     allowed_domains = ['https://www.superjob.ru']
-    start_urls = ['https://www.superjob.ru/vacancy/search/?keywords=Программист']
+    start_urls = ['https://www.superjob.ru/vakansii/programmist.html']
 
     def parse(self, response: HtmlResponse):  # Метод парсинга - точка входа для паука
-        next_page = response.css('a.L1p51::attr(href)').extract_first()  # Ссылка у кнопки "Далее"
-        yield response.follow(next_page, callback=self.parse)  # Переход на следующую страницу и вызов самой себя
+        next_page = response.css('a.f-test-link-dalshe::attr(href)').extract_first()  # Ссылка у кнопки "Далее"
+        yield response.follow(next_page, callback=self.parse, dont_filter=True)  # Переход на следующую страницу и вызов самой себя
         # Как только дошли до последней страницы идем дальше по коду
-        #vacancy = response.css(  # Ищем ссылку на вакансию и добавляем ее в наш список
-        #    'div.vacancy-serp div.vacancy-serp-item div.vacancy-serp-item__row_header a.bloko-link::attr(href)').extract()
-        #for link in vacancy:  # Переходим по ссылкам на вакансии из полученного списка
-        #    yield response.follow(link, callback=self.vacansy_parse)  # Вызываем метод для сбора инф-ции со страницы
+        vacancy = response.css(
+            'a.icMQ_._3dPok._1QIBo::attr(href)').extract()
+        for link in vacancy:  # Переходим по ссылкам на вакансии из полученного списка
+            link = 'https://www.superjob.ru' + link
+            yield response.follow(link, callback=self.vacansy_parse,dont_filter=True)  # Вызываем метод для сбора инф-ции со страницы
 
     def vacansy_parse(self, response: HtmlResponse):
-        #name = response.css('div.vacancy-title h1.header::text').extract_first()  # Наименование вакансии
-        #salary = response.css('div.vacancy-title p.vacancy-salary::text').extract_first()  # Зарплата
-        # создаем список элементов с информациям по зарплатам
-        #salary = re.sub('\xa0', "", salary)
-        #in_salary = self.get_min_salary(salary)
-        #max_salary = self.get_max_salary(salary)
-        #min_salary = self.clean_min_salary_text(min_salary)
-        #max_salary = self.clean_max_salary_text(max_salary)
+        name = response.css('h1.rFbjy::text').extract_first()  # Наименование вакансии
+        salary = response.css('span._2Wp8I').extract_first()  # Зарплата
+        salary = re.findall('<span>\w+ \w+<\/span>',salary)
         url = response.request.url
-        yield JobparserItem(url=url)  # Передаем сформированный item в pipeline
+        min_salary = '0'
+        max_salary = '0'
+        yield JobparserItem(name=name, min_salary=min_salary, max_salary=max_salary, url=url)  # Передаем сформированный item в pipeline
